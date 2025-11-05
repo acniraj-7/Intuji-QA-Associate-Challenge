b2dffcf14275e5fc0c181a432e83941b6e1ec5f1
Niraj <166219419+acniraj-7@users.noreply.github.com>
2025-11-05 10:54:12 +0545

    Update registration test and page object: improve structure, session handling, and verification

--- cypress/e2e/registration.cy.js
+++ cypress/e2e/registration.cy.js
@@ -1,52 +1,125 @@
+// import RegistrationPage from '../pages/RegistrationPage'
+// import { faker } from '@faker-js/faker'
+
+// describe('AutomationExercise Registration', () => {
+//   const regPage = new RegistrationPage()
+
+//   it('registers a new user successfully', () => {
+//     // Generate random user data using Faker
+//     const name = faker.name.firstName()
+//     const email = faker.internet.email()
+//     const password = faker.internet.password(8)
+//     const firstName = faker.name.firstName()
+//     const lastName = faker.name.lastName()
+//     const company = faker.company.name()
+//     const address1 = faker.address.streetAddress()
+//     const address2 = faker.address.secondaryAddress()
+//     const country = 'Canada' // Example, choose valid option from dropdown
+//     const state = faker.address.state()
+//     const city = faker.address.city()
+//     const zipcode = faker.address.zipCode()
+//     const mobile = faker.phone.number('##########')
+
+//     // Step 1: Signup
+//     regPage.visit()
+//     regPage.enterName(name)
+//     regPage.enterEmail(email)
+//     regPage.clickSignupButton()
+
+//     // Step 2: Fill account & address info
+//     regPage.selectTitle('Mr')
+//     regPage.enterPassword(password)
+//     regPage.selectDateOfBirth('10', 'May', '1990')
+//     regPage.checkNewsletter()
+//     regPage.checkOffers()
+//     regPage.enterFirstName(firstName)
+//     regPage.enterLastName(lastName)
+//     regPage.enterCompany(company)
+//     regPage.enterAddress1(address1)
+//     regPage.enterAddress2(address2)
+//     regPage.selectCountry(country)
+//     regPage.enterState(state)
+//     regPage.enterCity(city)
+//     regPage.enterZipcode(zipcode)
+//     regPage.enterMobileNumber(mobile)
+
+//     // Submit form and verify
+//     regPage.submitAccountCreation()
+//     regPage.verifyAccountCreated()
+//     regPage.continueAfterAccountCreated()
+//     regPage.verifyUserLoggedIn(name)
+//   })
+// })
+// cypress/e2e/registerUser.cy.js
+
 import RegistrationPage from '../pages/RegistrationPage'
 import { faker } from '@faker-js/faker'
 
 describe('AutomationExercise Registration', () => {
   const regPage = new RegistrationPage()
 
-  it('registers a new user successfully', () => {
+  // We'll store user data here to reuse in other tests
+  let user = {}
+
+  it('registers a new user successfully and saves session', () => {
     // Generate random user data using Faker
-    const name = faker.name.firstName()
-    const email = faker.internet.email()
-    const password = faker.internet.password(8)
-    const firstName = faker.name.firstName()
-    const lastName = faker.name.lastName()
-    const company = faker.company.name()
-    const address1 = faker.address.streetAddress()
-    const address2 = faker.address.secondaryAddress()
-    const country = 'Canada' // Example, choose valid option from dropdown
-    const state = faker.address.state()
-    const city = faker.address.city()
-    const zipcode = faker.address.zipCode()
-    const mobile = faker.phone.number('##########')
+    user = {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(8),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      company: faker.company.name(),
+      address1: faker.address.streetAddress(),
+      address2: faker.address.secondaryAddress(),
+      country: 'Canada', // Example, pick a valid dropdown option
+      state: faker.address.state(),
+      city: faker.address.city(),
+      zipcode: faker.address.zipCode(),
+      mobile: faker.phone.number('##########')
+    }
 
     // Step 1: Signup
     regPage.visit()
-    regPage.enterName(name)
-    regPage.enterEmail(email)
+    regPage.enterName(user.name)
+    regPage.enterEmail(user.email)
     regPage.clickSignupButton()
 
     // Step 2: Fill account & address info
     regPage.selectTitle('Mr')
-    regPage.enterPassword(password)
+    regPage.enterPassword(user.password)
     regPage.selectDateOfBirth('10', 'May', '1990')
     regPage.checkNewsletter()
     regPage.checkOffers()
-    regPage.enterFirstName(firstName)
-    regPage.enterLastName(lastName)
-    regPage.enterCompany(company)
-    regPage.enterAddress1(address1)
-    regPage.enterAddress2(address2)
-    regPage.selectCountry(country)
-    regPage.enterState(state)
-    regPage.enterCity(city)
-    regPage.enterZipcode(zipcode)
-    regPage.enterMobileNumber(mobile)
+    regPage.enterFirstName(user.firstName)
+    regPage.enterLastName(user.lastName)
+    regPage.enterCompany(user.company)
+    regPage.enterAddress1(user.address1)
+    regPage.enterAddress2(user.address2)
+    regPage.selectCountry(user.country)
+    regPage.enterState(user.state)
+    regPage.enterCity(user.city)
+    regPage.enterZipcode(user.zipcode)
+    regPage.enterMobileNumber(user.mobile)
 
     // Submit form and verify
     regPage.submitAccountCreation()
     regPage.verifyAccountCreated()
     regPage.continueAfterAccountCreated()
-    regPage.verifyUserLoggedIn(name)
+    regPage.verifyUserLoggedIn(user.name)
+
+    // Save session for reuse in other tests
+    cy.session(user.email, () => {
+      cy.visit('/login')
+      cy.get('input[data-qa="login-email"]').type(user.email)
+      cy.get('input[data-qa="login-password"]').type(user.password)
+      cy.get('button[data-qa="login-button"]').click()
+      regPage.verifyUserLoggedIn(user.name)
+    })
+  })
+
+  // Optionally, save user data in Cypress env to use in other specs
+  after(() => {
+    Cypress.env('registeredUser', user)
   })
 })
--- cypress/pages/RegistrationPage.js
+++ cypress/pages/RegistrationPage.js
@@ -1,83 +1,204 @@
+// // cypress/pages/RegistrationPage.js
+
+// /**
+//  * Page Object Model for AutomationExercise Registration Page
+//  * Handles signup, account creation, and user verification
+//  */
+// class RegistrationPage {
+
+//   /** Visit the login/signup page */
+//   visit() {
+//     cy.visit('/login') // baseUrl should be set in cypress.config.js
+//   }
+
+//   /** ------------------ Signup Form ------------------ */
+
+//   /** Enter full name in signup form */
+//   enterName(name) {
+//     cy.get('input[data-qa="signup-name"]').type(name)
+//   }
+
+//   /** Enter email in signup form */
+//   enterEmail(email) {
+//     cy.get('input[data-qa="signup-email"]').type(email)
+//   }
+
+//   /** Click the Signup button */
+//   clickSignupButton() {
+//     cy.get('button[data-qa="signup-button"]').click()
+//   }
+
+//   /** ------------------ Account Information Form ------------------ */
+
+//   /** Select title (Mr/Mrs) */
+//   selectTitle(title = 'Mr') {
+//     const genderId = title === 'Mr' ? 1 : 2
+//     cy.get(`input[id="id_gender${genderId}"]`).check()
+//   }
+
+//   /** Enter password */
+//   enterPassword(password) {
+//     cy.get('input[id="password"]').type(password)
+//   }
+
+//   /** Select date of birth */
+//   selectDateOfBirth(day, month, year) {
+//     cy.get('select[id="days"]').select(day)
+//     cy.get('select[id="months"]').select(month)
+//     cy.get('select[id="years"]').select(year)
+//   }
+
+//   /** Subscribe to newsletter */
+//   checkNewsletter() {
+//     cy.get('input[id="newsletter"]').check()
+//   }
+
+//   /** Opt-in for special offers */
+//   checkOffers() {
+//     cy.get('input[id="optin"]').check()
+//   }
+
+//   /** ------------------ Address Information ------------------ */
+
+//   enterFirstName(firstName) {
+//     cy.get('input[id="first_name"]').type(firstName)
+//   }
+
+//   enterLastName(lastName) {
+//     cy.get('input[id="last_name"]').type(lastName)
+//   }
+
+//   enterCompany(company) {
+//     cy.get('input[id="company"]').type(company)
+//   }
+
+//   enterAddress1(address1) {
+//     cy.get('input[id="address1"]').type(address1)
+//   }
+
+//   enterAddress2(address2) {
+//     cy.get('input[id="address2"]').type(address2)
+//   }
+
+//   selectCountry(country) {
+//     cy.get('select[id="country"]').select(country)
+//   }
+
+//   enterState(state) {
+//     cy.get('input[id="state"]').type(state)
+//   }
+
+//   enterCity(city) {
+//     cy.get('input[id="city"]').type(city)
+//   }
+
+//   enterZipcode(zipcode) {
+//     cy.get('input[id="zipcode"]').type(zipcode)
+//   }
+
+//   enterMobileNumber(mobile) {
+//     cy.get('input[id="mobile_number"]').type(mobile)
+//   }
+
+//   /** Submit account creation form */
+//   submitAccountCreation() {
+//     cy.get('button[data-qa="create-account"]').click()
+//   }
+
+//   /** ------------------ Verification Methods ------------------ */
+
+//   /** Verify that account is created successfully */
+//   verifyAccountCreated() {
+//     // Wait up to 10 seconds for the "Account Created!" heading to appear
+//     cy.get('h2.title.text-center', { timeout: 10000 })
+//       .should('contain.text', 'Account Created!')
+//   }
+
+//   /** Click Continue button after account creation */
+//   continueAfterAccountCreated() {
+//     cy.get('a[data-qa="continue-button"]').click()
+//   }
+
+//   /** Verify that user is logged in */
+//   verifyUserLoggedIn(name) {
+//     cy.contains(`Logged in as ${name}`, { timeout: 10000 }).should('be.visible')
+//   }
+// }
+
+// export default RegistrationPage
 // cypress/pages/RegistrationPage.js
 
 /**
  * Page Object Model for AutomationExercise Registration Page
- * Handles signup, account creation, and user verification
+ * Handles user signup, account creation, and session verification.
+ * Designed for readability, maintainability, and reusable methods.
  */
+
 class RegistrationPage {
-
-  /** Visit the login/signup page */
+  /** Visit the signup/login page */
   visit() {
-    cy.visit('/login') // baseUrl should be set in cypress.config.js
+    cy.visit('/login') // baseUrl is configured in cypress.config.js
   }
 
-  /** ------------------ Signup Form ------------------ */
+  /** ------------------ Signup Form Methods ------------------ */
 
-  /** Enter full name in signup form */
   enterName(name) {
-    cy.get('input[data-qa="signup-name"]').type(name)
+    cy.get('input[data-qa="signup-name"]').clear().type(name)
   }
 
-  /** Enter email in signup form */
   enterEmail(email) {
-    cy.get('input[data-qa="signup-email"]').type(email)
+    cy.get('input[data-qa="signup-email"]').clear().type(email)
   }
 
-  /** Click the Signup button */
   clickSignupButton() {
     cy.get('button[data-qa="signup-button"]').click()
   }
 
-  /** ------------------ Account Information Form ------------------ */
+  /** ------------------ Account Information Methods ------------------ */
 
-  /** Select title (Mr/Mrs) */
   selectTitle(title = 'Mr') {
     const genderId = title === 'Mr' ? 1 : 2
     cy.get(`input[id="id_gender${genderId}"]`).check()
   }
 
-  /** Enter password */
   enterPassword(password) {
-    cy.get('input[id="password"]').type(password)
+    cy.get('input[id="password"]').clear().type(password)
   }
 
-  /** Select date of birth */
   selectDateOfBirth(day, month, year) {
     cy.get('select[id="days"]').select(day)
     cy.get('select[id="months"]').select(month)
     cy.get('select[id="years"]').select(year)
   }
 
-  /** Subscribe to newsletter */
   checkNewsletter() {
     cy.get('input[id="newsletter"]').check()
   }
 
-  /** Opt-in for special offers */
   checkOffers() {
     cy.get('input[id="optin"]').check()
   }
 
-  /** ------------------ Address Information ------------------ */
+  /** ------------------ Address Information Methods ------------------ */
 
   enterFirstName(firstName) {
-    cy.get('input[id="first_name"]').type(firstName)
+    cy.get('input[id="first_name"]').clear().type(firstName)
   }
 
   enterLastName(lastName) {
-    cy.get('input[id="last_name"]').type(lastName)
+    cy.get('input[id="last_name"]').clear().type(lastName)
   }
 
   enterCompany(company) {
-    cy.get('input[id="company"]').type(company)
+    cy.get('input[id="company"]').clear().type(company)
   }
 
   enterAddress1(address1) {
-    cy.get('input[id="address1"]').type(address1)
+    cy.get('input[id="address1"]').clear().type(address1)
   }
 
   enterAddress2(address2) {
-    cy.get('input[id="address2"]').type(address2)
+    cy.get('input[id="address2"]').clear().type(address2)
   }
 
   selectCountry(country) {
@@ -85,43 +206,57 @@ class RegistrationPage {
   }
 
   enterState(state) {
-    cy.get('input[id="state"]').type(state)
+    cy.get('input[id="state"]').clear().type(state)
   }
 
   enterCity(city) {
-    cy.get('input[id="city"]').type(city)
+    cy.get('input[id="city"]').clear().type(city)
   }
 
   enterZipcode(zipcode) {
-    cy.get('input[id="zipcode"]').type(zipcode)
+    cy.get('input[id="zipcode"]').clear().type(zipcode)
   }
 
   enterMobileNumber(mobile) {
-    cy.get('input[id="mobile_number"]').type(mobile)
+    cy.get('input[id="mobile_number"]').clear().type(mobile)
   }
 
-  /** Submit account creation form */
+  /** Submit the account creation form */
   submitAccountCreation() {
     cy.get('button[data-qa="create-account"]').click()
   }
 
   /** ------------------ Verification Methods ------------------ */
 
-  /** Verify that account is created successfully */
   verifyAccountCreated() {
-    // Wait up to 10 seconds for the "Account Created!" heading to appear
+    // Wait up to 10 seconds for confirmation message
     cy.get('h2.title.text-center', { timeout: 10000 })
       .should('contain.text', 'Account Created!')
   }
 
-  /** Click Continue button after account creation */
   continueAfterAccountCreated() {
     cy.get('a[data-qa="continue-button"]').click()
   }
 
-  /** Verify that user is logged in */
   verifyUserLoggedIn(name) {
     cy.contains(`Logged in as ${name}`, { timeout: 10000 }).should('be.visible')
+  }
+
+  /** ------------------ Session Helpers ------------------ */
+
+  saveSession() {
+    // Save cookies for reuse in future tests
+    cy.getCookies().then(cookies => {
+      cy.wrap(cookies).as('userCookies')
+    })
+  }
+
+  restoreSession() {
+    cy.get('@userCookies').then(cookies => {
+      cookies.forEach(cookie => {
+        cy.setCookie(cookie.name, cookie.value)
+      })
+    })
   }
 }
 
